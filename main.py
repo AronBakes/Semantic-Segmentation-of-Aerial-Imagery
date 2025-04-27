13dc6d6afed1bf530b635bc3495c7d6747cddc90
AronBakes <aronbakes@gmail.com>
2025-04-28 06:44:05 +1000

    Renamed some files

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # main.py
 
-from libs import training_keras
+from libs import training
 from libs import datasets
-from libs import models_keras
-from libs import inference_keras
+from libs import models
+from libs import inference
 from libs import scoring
+from libs import util
 
 import wandb
 import argparse
@@ -39,17 +40,17 @@ if __name__ == '__main__':
     }[args.input_type]
 
     if args.model == 'unet':
-        model = models_keras.build_unet(input_shape=(256, 256, input_channels), num_classes=7)
+        model = models.build_unet(input_shape=(256, 256, input_channels), num_classes=7)
     elif args.model == 'segformer':
-        model = models_keras.build_segformer(input_shape=(256, 256, input_channels), num_classes=7)
+        model = models.build_segformer(input_shape=(256, 256, input_channels), num_classes=7)
     else:
         raise ValueError("Invalid model choice.")
 
     # --- Train Model ---
-    training_keras.train_model(args.dataset, model, input_type=args.input_type)
+    training.train_model(args.dataset, model, input_type=args.input_type)
 
     # --- Run Inference ---
-    inference_keras.run_inference(args.dataset, model=model, basedir=wandb.run.dir)
+    inference.run_inference(args.dataset, model=model, basedir=wandb.run.dir)
 
     # --- Score Predictions ---
     score, _ = scoring.score_predictions(args.dataset, basedir=wandb.run.dir)
